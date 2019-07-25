d0a7f35cfa51538f4ed783445c0a81950cd455fe
grevelle <31642433+grevelle@users.noreply.github.com>
2019-07-24 23:02:09 -0500

    Test

--- rain-bypass-0.5.py
+++ rain-bypass-0.5.py
@@ -1,27 +1,3 @@
-"""
-Order of program
-Open libraries and Config File
-Determine Lat/Lon from Google
-    Input
-        API Keys
-    Output Lat/Lon
-Get precip amount from DarkSky
-    Input
-        API Keys
-        Last weather update
-        Number of weather updates per day (checkIncrement)
-    Output
-        Precip amount over time period
-Set status
-    Output
-        Watering enabled/disabled
-Set GPIO Pins
-    Input
-        GPIO Pins
-    Output
-        GPIO Pins
-"""
-
 import configparser
 import json
 import time
